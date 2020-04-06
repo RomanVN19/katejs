fc0ab5b80f094bd670b9e59d54f7958f9d624ec9
romannep <neproman@gmail.com>
2020-04-06 20:37:40 +0400

    recordsPut, beforeRecordsPut

--- src/katejs-modules/katejs-doc/lib/entities/DocMixin.js
+++ src/katejs-modules/katejs-doc/lib/entities/DocMixin.js
@@ -59,10 +59,6 @@ const DocMixin = Entity => class DocEntity extends Entity {
           ...record,
         }));
         promises.push(this.app[recordEntity].recordsPut({ records, transaction }));
-        // need to use put to process hooks like serviceAccount
-        // promises.push(this.app[recordEntity][model].bulkCreate(records, { transaction }));
-        // records.forEach(record => promises.push(this.app[recordEntity]
-        //   .put({ data: { body: record }, transaction, ctx })));
       });
       await Promise.all(promises);
     }
