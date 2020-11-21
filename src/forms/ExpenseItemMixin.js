1883e6a07c902b3e06e9cbf26236159d9e4cafe6
romannep <neproman@gmail.com>
2020-11-21 20:25:29 +0400

    solve #9

--- src/forms/ExpenseItemMixin.js
+++ src/forms/ExpenseItemMixin.js
@@ -1,11 +1,18 @@
+import { Elements } from 'katejs/lib/client';
 
 export default ItemForm => class ExpenseItem extends ItemForm {
   static doc = true;
 
   constructor(args) {
     super(args);
-
     this.elements.get('total').disabled = true;
+    this.elements.unshift({
+      type: Elements.GRID,
+      elements: [
+        this.elements.cut('wallet'),
+        this.elements.cut('total',)
+      ],
+    });
     const table = this.elements.get('expensesDetails');
     const sumCol = table.columns.find(col => col.id === 'sum');
     sumCol.onChange = () => this.calcTotal();
