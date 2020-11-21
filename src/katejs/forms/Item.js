6625f9f8f4d836140efe680b25af6d8043072004
romannep <neproman@gmail.com>
2020-11-21 20:06:53 +0400

    solve #10

--- src/katejs/forms/Item.js
+++ src/katejs/forms/Item.js
@@ -3,11 +3,6 @@ import Form from './Form';
 import { ConfirmDialog } from './Dialogs';
 import { getElement, getTableElement, makeTitle } from '../shared';
 
-const ok = Symbol('ok');
-const load = Symbol('load');
-const save = Symbol('save');
-const del = Symbol('delete');
-const close = Symbol('close');
 
 const makeItemForm = ({ structure, name, addActions = true, addElements = true }) =>
   class ItemForm extends Form {
@@ -37,34 +32,28 @@ const makeItemForm = ({ structure, name, addActions = true, addElements = true }
             id: '__OK',
             type: Elements.BUTTON,
             title: 'OK',
-            onClick: this[ok],
+            onClick: () => this.ok(),
             disabled: false,
           },
           {
             id: '__Save',
             type: Elements.BUTTON,
             title: 'Save',
-            onClick: this[save],
+            onClick: () => this.save(),
             disabled: false,
           },
-          // {
-          //   id: '__Load',
-          //   type: Elements.BUTTON,
-          //   title: 'Load',
-          //   onClick: this.load,
-          // },
           {
             id: '__Delete',
             type: Elements.BUTTON,
             title: 'Delete',
-            onClick: this[del],
+            onClick: () => this.delete(),
             disabled: false,
           },
           {
             id: '__Close',
             type: Elements.BUTTON,
             title: 'Close',
-            onClick: this[close],
+            onClick: () => this.close(),
             disabled: false,
           },
         ];
@@ -101,23 +90,20 @@ const makeItemForm = ({ structure, name, addActions = true, addElements = true }
       if (!await this.content.confirmDialog.confirm({ title: 'Are you sure?' })) return;
       const result = await this.app[name].delete({ uuid: this.uuid });
       if (result.response) {
-        this[close]();
+        this.close();
         this.app.showAlert({ type: 'success', title: 'Deleted!' });
       }
       if (result.error) {
         this.app.showAlert({ type: 'warning', title: result.error.message });
       }
     }
-    [close] = () => {
+    close () {
       this.app.open(`${name}List`, {});
     };
-    [ok] = async () => {
+    async ok() {
       await this.save();
-      this[close]();
+      this.close();
     };
-    [load] = () => this.load();
-    [save] = () => this.save();
-    [del] = () => this.delete();
   };
 
 export default makeItemForm;
