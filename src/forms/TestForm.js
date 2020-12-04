9b15e9f792840bc46cafc8241d50b0e723154db5
romannep <neproman@gmail.com>
2020-12-04 23:26:01 +0400

    solve #12

--- src/forms/TestForm.js
+++ src/forms/TestForm.js
@@ -33,8 +33,32 @@ export default class TestForm extends Form {
           { test: 123 },
         ],
       },
+      {
+        type: Elements.GRID,
+        elements: [
+          {
+            id: 'select',
+            type: Elements.SELECT,
+            selectValue: true,
+            options: [],
+            value: 2,
+          },
+          {
+            type: Elements.BUTTON,
+            title: 'set 3',
+            onClick: () => this.content.select.value = 3,
+          },
+        ],
+      },
     ];
 
     console.log('app form constr', !!this.app.authorization, this.app.allow('Expense', 'put'));
+  }
+  afterInit() {
+    this.content.select.options = [
+      { title: 'v1', value: 1 },
+      { title: 'v2', value: 2 },
+      { title: 'v3', value: 3 },
+    ];
   }
 }
